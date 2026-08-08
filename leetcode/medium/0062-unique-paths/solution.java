410cdc23364e4bd01c207ab2ed7aeb2dc9a9402a
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-08-08 11:39:40 +0530

    ✅ Unique Paths | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 42 MB | 2026-08-08

--- leetcode/medium/0062-unique-paths/solution.java
+++ leetcode/medium/0062-unique-paths/solution.java
@@ -1,15 +1,19 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++)
-            dp[i][0]=1;
-        for(int i=0;i<n;i++)
-            dp[0][i]=1;
-        for(int i=1;i<m;i++) {
-            for(int j=1;j<n;j++) {
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        int[] prev=new int[n];
+        for(int i=0;i<m;i++) {
+            int[] temp=new int[n];
+            for(int j=0;j<n;j++) {
+                if(i==0 && j==0) {
+                    temp[j]=1;
+                    continue;
+                }
+                int up=(i>0)?prev[j]:0;
+                int left=(j>0)?temp[j-1]:0;
+                temp[j]=up+left;
             }
+            prev=temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 }
