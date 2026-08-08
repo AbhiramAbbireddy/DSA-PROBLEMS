2f5845c0f1ce52e29885d5d8bb960b01a153b7d4
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-08-08 12:14:22 +0530

    ✅ Minimum Path Sum | Medium | Java | Runtime: 6 ms (10.22%) | Memory: 50.4 MB | 2026-08-08

--- leetcode/medium/0064-minimum-path-sum/solution.java
+++ leetcode/medium/0064-minimum-path-sum/solution.java
@@ -1,16 +1,21 @@
 class Solution {
-    private int solve(int i,int j,int[][] grid,int[][] dp) {
-        if(i==0 && j==0) return grid[0][0];
-        if(i<0 || j<0) return 1_000_000_000;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up=grid[i][j]+solve(i-1,j,grid,dp);
-        int left=grid[i][j]+solve(i,j-1,grid,dp);
-        return dp[i][j]=Math.min(up,left);
-    }
     public int minPathSum(int[][] grid) {
-        int[][] dp=new int[grid.length][grid[0].length];
-        for(int[] r: dp)
-            Arrays.fill(r,-1);
-        return solve(grid.length-1,grid[0].length-1,grid,dp);
+        int m=grid.length,n=grid[0].length;
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(i==0 && j==0) dp[i][j]=grid[0][0];
+                else {
+                    int up=grid[i][j];
+                    if(i>0) up+=dp[i-1][j];
+                    else up+=(int) 1e9;
+                    int left=grid[i][j];
+                    if(j>0) left+=dp[i][j-1];
+                    else left+=(int) 1e9;
+                    dp[i][j]=Math.min(up,left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
