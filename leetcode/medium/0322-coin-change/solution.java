2a990f99bc70135a3cefc2cc94ddf7479d8fa1a4
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-08-11 07:10:42 +0530

    ✅ Coin Change | Medium | Java | Runtime: 16 ms (68.82%) | Memory: 46.4 MB | 2026-08-11

--- leetcode/medium/0322-coin-change/solution.java
+++ leetcode/medium/0322-coin-change/solution.java
@@ -1,20 +1,16 @@
 class Solution {
     private static final int INF=1_000_000;
-    private int solve(int[] coins, int amount, int[] dp) {
-        if(amount==0) return 0;
-        if(amount<0) return INF;
-        if(dp[amount]!=-1) return dp[amount];
-        int minamount=INF;
-        for(int i=0;i<coins.length;i++) {
-            minamount=Math.min(minamount,solve(coins,amount-coins[i],dp));
-        }
-        return dp[amount]=1+minamount;
-    }
     public int coinChange(int[] coins, int amount) {
         int[] dp=new int[amount+1];
-        Arrays.fill(dp,-1);
+        Arrays.fill(dp,INF);
         dp[0]=0;
-        int res=solve(coins,amount,dp);
-        return res>=INF?-1:res;
+        for(int c=1;c<=amount;c++) {
+            for(int coin: coins) {
+                if(coin<=c) {
+                    dp[c]=Math.min(dp[c],dp[c-coin]+1);
+                }
+            }
+        }
+        return dp[amount]>=INF?-1:dp[amount];
     }
 }
