b2861640a94b64c6226823b95c2c92bc41642747
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-08-13 15:29:23 +0530

    ✅ Longest Substring of One Repeating Character | Hard | Java | Runtime: 127 ms (31.25%) | Memory: 148 MB | 2026-08-13

---
+++ leetcode/hard/2213-longest-substring-of-one-repeating-character/solution.java
@@ -0,0 +1,71 @@
+class Solution {
+    class Node {
+        char leftChar,rightChar;
+        int prefix,suffix;
+        int len;
+        int best;
+    }
+    Node[] tree;
+    char[] arr;
+    private void build(int idx,int l,int r) {
+        if(l==r) {
+            tree[idx]=new Node();
+            tree[idx].leftChar=arr[l];
+            tree[idx].rightChar=arr[r];
+            tree[idx].prefix=1;
+            tree[idx].suffix=1;
+            tree[idx].len=1;
+            tree[idx].best=1;
+            return;
+        }
+        int mid=l+(r-l)/2;
+        build(2*idx,l,mid);
+        build(2*idx+1,mid+1,r);
+        tree[idx]=merge(tree[2*idx],tree[2*idx+1]);
+    }
+    private Node merge(Node left,Node right) {
+        Node parent=new Node();
+        parent.len=left.len+right.len;
+        parent.leftChar=left.leftChar;
+        parent.rightChar=right.rightChar;
+        parent.prefix=left.prefix;
+        if(left.prefix==left.len && left.rightChar==right.leftChar)
+            parent.prefix=left.len+right.prefix;
+        parent.suffix=right.suffix;
+        if(right.suffix==right.len && left.rightChar==right.leftChar)
+            parent.suffix=right.len+left.suffix;
+        parent.best=Math.max(left.best,right.best);
+        if(left.rightChar==right.leftChar)
+            parent.best=Math.max(parent.best,left.suffix+right.prefix);
+        return parent;
+    }
+    private void update(int idx,int l,int r,int pos,char c) {
+        if(l==r) {
+            arr[pos]=c;
+            tree[idx].leftChar=c;
+            tree[idx].rightChar=c;
+            tree[idx].prefix=1;
+            tree[idx].suffix=1;
+            tree[idx].best=1;
+            return;
+        }
+        int mid=l+(r-l)/2;
+        if(pos<=mid)
+            update(2*idx,l,mid,pos,c);
+        else
+            update(2*idx+1,mid+1,r,pos,c);
+        tree[idx]=merge(tree[2*idx],tree[2*idx+1]);
+    }
+    public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
+        arr=s.toCharArray();
+        int n=arr.length;
+        tree=new Node[4*n];
+        build(1,0,n-1);
+        int[] res=new int[queryIndices.length];
+        for(int i=0;i<res.length;i++) {
+            update(1,0,n-1,queryIndices[i],queryCharacters.charAt(i));
+            res[i]=tree[1].best;
+        }
+        return res;
+    }
+}
