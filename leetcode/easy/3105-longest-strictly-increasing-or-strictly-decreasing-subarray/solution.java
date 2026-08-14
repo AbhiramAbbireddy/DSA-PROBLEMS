5c1717b83a799c7afb077ea8e5311bd05aafc6ca
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-08-14 06:48:56 +0530

    ✅ Longest Strictly Increasing or Strictly Decreasing Subarray | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 44.2 MB | 2026-08-14

---
+++ leetcode/easy/3105-longest-strictly-increasing-or-strictly-decreasing-subarray/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        int maxLen=1,currLen=1;
+        for(int i=1;i<nums.length;i++) {
+            if(nums[i]>nums[i-1]) currLen++;
+            else currLen=1;
+            maxLen=Math.max(maxLen,currLen);
+        }
+        currLen=1;
+        for(int i=1;i<nums.length;i++) {
+            if(nums[i]<nums[i-1]) currLen++;
+            else currLen=1;
+            maxLen=Math.max(maxLen,currLen);
+        }
+        return maxLen;
+    }
+}
