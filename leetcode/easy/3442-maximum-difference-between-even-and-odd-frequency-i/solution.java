3bb2b0828eae39e31b4d199ee1a3ccb9cda54832
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-08-14 05:58:07 +0530

    ✅ Maximum Difference Between Even and Odd Frequency I | Easy | Java | Runtime: 4 ms (15.47%) | Memory: 43.7 MB | 2026-08-14

--- leetcode/easy/3442-maximum-difference-between-even-and-odd-frequency-i/solution.java
+++ leetcode/easy/3442-maximum-difference-between-even-and-odd-frequency-i/solution.java
@@ -1,12 +1,13 @@
 class Solution {
     public int maxDifference(String s) {
         int[] freq=new int[26];
-        for(char c: s.toCharArray()) freq[c-'a']++;
-        int largest=Integer.MIN_VALUE,small=Integer.MAX_VALUE;
-        for(int i=0;i<26;i++) {
-            if(freq[i]!=0 && (freq[i]&1)==1) largest=Math.max(largest,freq[i]);
-            if(freq[i]!=0 && (freq[i]&1)==0) small=Math.min(small,freq[i]);
+        s.chars().forEach(c->freq[c-'a']++);
+        int maxOdd=0,minEven=Integer.MAX_VALUE;
+        for(int count: freq) {
+            if(count==0) continue;
+            if((count&1)==1) maxOdd=Math.max(maxOdd,count);
+            else minEven=Math.min(minEven,count);
         }
-        return largest-small;
+        return maxOdd-minEven;
     }
 }
