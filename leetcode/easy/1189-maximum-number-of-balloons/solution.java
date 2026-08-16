d671718bd14fcb97ba4381ff8108e51a68e2fd30
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-08-16 16:47:39 +0530

    ✅ Maximum Number of Balloons | Easy | Java | Runtime: 2 ms (93.03%) | Memory: 43 MB | 2026-08-16

---
+++ leetcode/easy/1189-maximum-number-of-balloons/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] freq=new int[26];
+        for(char c: text.toCharArray()) freq[c-'a']++;
+        freq['l'-'a']/=2;
+        freq['o'-'a']/=2;
+        return Math.min(
+            Math.min(freq['b' - 'a'], freq['a' - 'a']),
+            Math.min(freq['l' - 'a'],
+                     Math.min(freq['o' - 'a'], freq['n' - 'a']))
+        );
+    }
+}
