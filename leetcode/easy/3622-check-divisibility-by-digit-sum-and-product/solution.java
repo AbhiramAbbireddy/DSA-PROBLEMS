74ff3af7a9a3cfbb4ae7f81df172fc13e56e3ff3
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-08-22 13:58:52 +0530

    ✅ Check Divisibility by Digit Sum and Product | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 42 MB | 2026-08-22

---
+++ leetcode/easy/3622-check-divisibility-by-digit-sum-and-product/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int sum=0,prod=1;
+        int temp=n;
+        while(temp>0) {
+            int d=temp%10;
+            sum+=d;
+            prod*=d;
+            temp/=10;
+        }
+        return n%(sum+prod)==0;
+    }
+}
