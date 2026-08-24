7b18e62c9121aa7f05040e4a66ab08e81a305af1
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-08-25 04:40:02 +0530

    ✅ Largest 3-Same-Digit Number in String | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 43.2 MB | 2026-08-24

--- leetcode/easy/2264-largest-3-same-digit-number-in-string/solution.java
+++ leetcode/easy/2264-largest-3-same-digit-number-in-string/solution.java
@@ -1,23 +1,15 @@
 class Solution {
     public String largestGoodInteger(String num) {
-        int n=num.length();
-        Map<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<3;i++) {
-            char c=num.charAt(i);
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
-        String ans="";
-        if(map.size()==1) ans=num.substring(0,3);
-        for(int i=1;i<=n-3;i++) {
-            char a=num.charAt(i-1),b=num.charAt(i+2);
-            map.put(a,map.get(a)-1);
-            if(map.get(a)==0) map.remove(a);
-            map.put(b,map.getOrDefault(b,0)+1);
-            if(map.size()==1) {
-                String curr=num.substring(i,i+3);
-                if(ans.equals("") || curr.compareTo(ans)>0) ans=curr;
-            }
-        }
-        return ans;
+        if(num.contains("999"))return "999";
+        if(num.contains("888"))return "888";
+        if(num.contains("777"))return "777";
+        if(num.contains("666"))return "666";
+        if(num.contains("555"))return "555";
+        if(num.contains("444"))return "444";
+        if(num.contains("333"))return "333";
+        if(num.contains("222"))return "222";
+        if(num.contains("111"))return "111";
+        if(num.contains("000"))return "000";
+        return "";
     }
 }
