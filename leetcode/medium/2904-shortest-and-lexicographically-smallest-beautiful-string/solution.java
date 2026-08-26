77a1d719f0266f0194a8125982e4be9c6ed15ce5
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-08-26 09:51:18 +0530

    ✅ Shortest and Lexicographically Smallest Beautiful String | Medium | Java | Runtime: 2 ms (40.94%) | Memory: 44.3 MB | 2026-08-26

---
+++ leetcode/medium/2904-shortest-and-lexicographically-smallest-beautiful-string/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        int n=s.length();
+        int left=0,ones=0;
+        String beautiful="";
+        for(int right=0;right<n;right++) {
+            if(s.charAt(right)=='1') ones++;
+            while(ones>k) {
+                if(s.charAt(left)=='1') ones--;
+                left++;
+            }
+            while(ones==k && s.charAt(left)=='0') left++;
+            if(ones==k) {
+                String cur=s.substring(left,right+1);
+                if(beautiful.isEmpty() ||
+                cur.length()<beautiful.length() 
+                || (cur.length()==beautiful.length() && cur.compareTo(beautiful)<0)) 
+                    beautiful=cur;
+            }
+        }
+        return beautiful;
+    }
+}
