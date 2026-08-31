e7463d8be555be215b84ea925a14ad57f69ebae3
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-08-31 19:49:32 +0530

    ✅ Number of Subsequences That Satisfy the Given Sum Condition | Medium | Java | Runtime: 33 ms (58.05%) | Memory: 66.6 MB | 2026-08-31

---
+++ leetcode/medium/1498-number-of-subsequences-that-satisfy-the-given-sum-condition/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        Arrays.sort(nums);
+        int count=0;
+        int n=nums.length,mod=1_000_000_007;
+        int[] pow=new int[n];
+        pow[0]=1;
+        for(int i=1;i<n;i++) pow[i]=(pow[i-1]*2)%mod;
+        int left=0,right=n-1;
+        while(left<=right) {
+            if(nums[left]+nums[right]<=target) {
+                count+=pow[right-left];
+                count%=mod;
+                left++;
+            } else right--;
+        }
+        return count;
+    }
+}
