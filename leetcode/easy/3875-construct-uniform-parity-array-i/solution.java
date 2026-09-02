7f34a403adbc2727f1fa6cb33f6a8fb7c046514d
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-09-02 19:32:52 +0530

    ✅ Construct Uniform Parity Array I | Easy | Java | Runtime: 1 ms (31.89%) | Memory: 45 MB | 2026-09-02

--- leetcode/easy/3875-construct-uniform-parity-array-i/solution.java
+++ leetcode/easy/3875-construct-uniform-parity-array-i/solution.java
@@ -1,43 +1,12 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n=nums1.length;
-        boolean even=true,odd=true;
-        for(int i=0;i<n;i++) {
-            if((nums1[i]&1)==1) {
-                boolean found=false;
-                for(int j=0;j<n;j++) {
-                    if(j==i) continue;
-                    int d=nums1[i]-nums1[j];
-                    if((d&1)==0) {
-                        found=true;
-                        break;
-                    }
-                }
-                if(!found){
-                    even=false;
-                    break;
-                } 
-                    
-            }
+        int odd=0,even=0;
+        for(int x: nums1) {
+            if((x&1)==1) odd++;
+            else even++;
         }
-        for(int i=0;i<n;i++) {
-            if((nums1[i]&1)==0) {
-                boolean found=false;
-                for(int j=0;j<n;j++) {
-                    if(j==i) continue;
-                    int d=nums1[i]-nums1[j];
-                    if((d&1)==1) {
-                        found=true;
-                        break;
-                    }
-                }
-                if(!found){
-                    odd=false;
-                    break;
-                } 
-                    
-            }
-        }
-        return even || odd;
+        boolean allEven=(odd==0 || odd>=2);
+        boolean allOdd=(even==0 || odd>=1);
+        return allEven || allOdd;
     }
 }
