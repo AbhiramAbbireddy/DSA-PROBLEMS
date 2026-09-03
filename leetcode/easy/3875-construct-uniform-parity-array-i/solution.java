6856e9aa29cf4ce03fb4580c038c266a9c99894a
A N VENKATA VEERA ABHIRAM <161499134+AbhiramAbbireddy@users.noreply.github.com>
2026-09-03 06:04:07 +0530

    ✅ Construct Uniform Parity Array I | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 44.6 MB | 2026-09-03

--- leetcode/easy/3875-construct-uniform-parity-array-i/solution.java
+++ leetcode/easy/3875-construct-uniform-parity-array-i/solution.java
@@ -1,12 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int odd=0,even=0;
-        for(int x: nums1) {
-            if((x&1)==1) odd++;
-            else even++;
-        }
-        boolean allEven=(odd==0 || odd>=2);
-        boolean allOdd=(even==0 || odd>=1);
-        return allEven || allOdd;
+        return true;
     }
 }
